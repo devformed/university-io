e245ed62e87ac5a25ee1cafa1a1eae01d796556a
TomTee557 <119242041+TomTee557@users.noreply.github.com>
2025-06-12 21:14:39 +0200

    Modifying Navbar.tsx component and adding default avatar

--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Views } from 'Enums/Views';
 import { NavbarPropsTypes } from './types/NavbarPropsTypes';
+import DefaultAvatar from 'Assets/Images/DefaultAvatar.png';
+
+import './styles/Navbar.scss';
 
 const Navbar = ({ onNavigate }: NavbarPropsTypes) => {
   return (
     <nav className="navbar">
-      <button className="navbar__button" onClick={() => onNavigate(Views.WELCOME)}>Strona główna</button>
-      <button className="navbar__button" onClick={() => onNavigate(Views.POINTS)}>Punkty</button>
-      <button className="navbar__button" onClick={() => onNavigate(Views.PACKAGES)}>Moje Przesyłki</button>
+        <div className="navbar__left">
+        <img
+          src={DefaultAvatar}
+          alt="avatar"
+          className="navbar__avatar"
+        />
+        <span className="navbar__username">Tomasz</span>
+      </div>
+      <div className="navbar__right">
+        <button className="navbar__button" onClick={() => onNavigate(Views.WELCOME)}>Strona główna</button>
+        <button className="navbar__button" onClick={() => onNavigate(Views.POINTS)}>Punkty</button>
+        <button className="navbar__button" onClick={() => onNavigate(Views.PACKAGES)}>Moje Przesyłki</button>
+      </div>
     </nav>
   );
 };
